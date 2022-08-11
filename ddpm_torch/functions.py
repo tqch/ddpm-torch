fix toy submodule: - add missing data attribute for swissroll dataset - reduce default time steps and increase number of temporal layers for   all toy experiments - add precision & cutoff arguments to kl divergence related function

ddpm_torch/functions.py
@@ -2,6 +2,7 @@ import math
 import torch
 import torch.nn.functional as F
 import numpy as np
+from typing import Union, Tuple
 
 
 DEFAULT_DTYPE = torch.float32
@@ -34,17 +35,22 @@ def approx_std_normal_cdf(x):
     return 0.5 * (1. + torch.tanh(math.sqrt(2. / math.pi) * (x + 0.044715 * torch.pow(x, 3))))
 
 
-def discretized_gaussian_loglik(x, means, log_scale):
+@torch.jit.script
+def discretized_gaussian_loglik(
+        x, means, log_scale, precision: float = 1./255,
+        cutoff: Union[float, Tuple[float, float]] = (-0.999, 0.999), tol: float = 1e-12):
+    if isinstance(cutoff, float):
+        cutoff = (-cutoff, cutoff)
     # Assumes data is integers [0, 255] rescaled to [-1, 1]
     x_centered = x - means
     inv_stdv = torch.exp(-log_scale)
-    upper = inv_stdv * (x_centered + 1./255)
+    upper = inv_stdv * (x_centered + precision)
     cdf_upper = torch.where(
-        x > 0.999, torch.as_tensor(1, dtype=torch.float32, device=x.device), approx_std_normal_cdf(upper))
-    lower = inv_stdv * (x_centered - 1./255)
+        x > cutoff[1], torch.as_tensor(1, dtype=torch.float32, device=x.device), approx_std_normal_cdf(upper))
+    lower = inv_stdv * (x_centered - precision)
     cdf_lower = torch.where(
-        x < -0.999, torch.as_tensor(0, dtype=torch.float32, device=x.device), approx_std_normal_cdf(lower))
-    log_probs = torch.log(torch.clamp(cdf_upper - cdf_lower - 1e-12, min=0).add(1e-12))
+        x < cutoff[0], torch.as_tensor(0, dtype=torch.float32, device=x.device), approx_std_normal_cdf(lower))
+    log_probs = torch.log(torch.clamp(cdf_upper - cdf_lower - tol, min=0).add(tol))
     return log_probs
 
 
@@ -70,4 +76,3 @@ def hist2d(data, bins, value_range=None):
     x, y = np.split(data, 2, axis=1)
     x, y = x.squeeze(1), y.squeeze(1)
     return np.histogram2d(x, y, bins=bins, range=value_range)[0]
-

ddpm_torch/toy/diffusion.py
@@ -45,7 +45,8 @@ class GaussianDiffusion:
             betas,
             model_mean_type,
             model_var_type,
-            loss_type
+            loss_type,
+            cutoff
     ):
         assert isinstance(betas, np.ndarray) and betas.dtype == np.float64
         assert (betas > 0).all() and (betas <= 1).all()
@@ -53,9 +54,9 @@ class GaussianDiffusion:
         self.model_mean_type = model_mean_type
         self.model_var_type = model_var_type
         self.loss_type = loss_type
+        self.cutoff = cutoff
 
-        timesteps = len(betas)
-        self.timesteps = timesteps
+        self.timesteps = len(betas)
 
         alphas = 1 - betas
         self.alphas_bar = np.cumprod(alphas)
@@ -215,7 +216,8 @@ class GaussianDiffusion:
             denoise_fn, x_t=x_t, t=t, clip_denoised=clip_denoised, return_pred=True)
         kl = normal_kl(true_mean, true_logvar, model_mean, model_logvar)
         kl = flat_mean(kl) / np.log(2.)  # natural base to base 2
-        decoder_nll = -discretized_gaussian_loglik(x_0, model_mean, log_scale=0.5 * model_logvar)
+        decoder_nll = -discretized_gaussian_loglik(
+            x_0, model_mean, log_scale=0.5 * model_logvar, cutoff=self.cutoff)
         decoder_nll = flat_mean(decoder_nll) / np.log(2.)
         output = torch.where(t.to(kl.device) > 0, kl, decoder_nll)
         return (output, pred_x_0) if return_pred else output
@@ -229,7 +231,8 @@ class GaussianDiffusion:
         # kl: weighted
         # mse: unweighted
         if self.loss_type == "kl":
-            losses = self._loss_term_bpd(denoise_fn, x_0=x_0, x_t=x_t, t=t, clip_denoised=False, return_pred=False)
+            losses = self._loss_term_bpd(
+                denoise_fn, x_0=x_0, x_t=x_t, t=t, clip_denoised=False, return_pred=False)
         elif self.loss_type == "mse":
             assert self.model_var_type != "learned"
             if self.model_mean_type == "mean":

train_toy.py
@@ -20,10 +20,10 @@ if __name__ == "__main__":
     parser.add_argument("--beta2", default=0.999, type=float, help="beta_2 in Adam")
     parser.add_argument("--lr-warmup", default=0, type=int, help="number of warming-up epochs")
     parser.add_argument("--batch-size", default=1000, type=int)
-    parser.add_argument("--timesteps", default=1000, type=int, help="number of diffusion steps")
+    parser.add_argument("--timesteps", default=100, type=int, help="number of diffusion steps")
     parser.add_argument("--beta-schedule", choices=["quad", "linear", "warmup10", "warmup50", "jsd"], default="linear")
-    parser.add_argument("--beta-start", default=0.0001, type=float)
-    parser.add_argument("--beta-end", default=0.02, type=float)
+    parser.add_argument("--beta-start", default=0.001, type=float)
+    parser.add_argument("--beta-end", default=0.2, type=float)
     parser.add_argument("--model-mean-type", choices=["mean", "x_0", "eps"], default="eps", type=str)
     parser.add_argument("--model-var-type", choices=["learned", "fixed-small", "fixed-large"], default="fixed-large", type=str)
     parser.add_argument("--loss-type", choices=["kl", "mse"], default="mse", type=str)
@@ -35,7 +35,7 @@ if __name__ == "__main__":
     parser.add_argument("--resume", action="store_true", help="to resume from a checkpoint")
     parser.add_argument("--gpu", default=0, type=int)
     parser.add_argument("--mid-features", default=128, type=int)
-    parser.add_argument("--num-temporal-layers", default=1, type=int)
+    parser.add_argument("--num-temporal-layers", default=3, type=int)
 
     args = parser.parse_args()
 
@@ -50,6 +50,7 @@ if __name__ == "__main__":
     batch_size = args.batch_size
     num_batches = data_size // batch_size
     trainloader = DataStreamer(dataset, batch_size=batch_size, num_batches=num_batches)
+    cutoff = {"gaussian8": 1.44, "gaussian25": 1.45, "swissroll": (-1.7, 2.2)}[dataset]
 
     # training parameters
     device = torch.device(f"cuda:{args.gpu}" if torch.cuda.is_available() else "cpu")
@@ -65,7 +66,7 @@ if __name__ == "__main__":
     model_var_type = args.model_var_type
     loss_type = args.loss_type
     diffusion = GaussianDiffusion(
-        betas=betas, model_mean_type=model_mean_type, model_var_type=model_var_type, loss_type=loss_type)
+        betas=betas, model_mean_type=model_mean_type, model_var_type=model_var_type, loss_type=loss_type, cutoff=cutoff)
 
     # model parameters
     out_features = 2 * in_features if model_var_type == "learned" else in_features